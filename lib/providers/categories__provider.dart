7647dc72a6a2695e6236a5557c63ed5f2c99d199
hasanbektaş1 <hasanbktas1@gmail.com>
2023-12-12 01:50:17 +0300

    içecekler kategorisi eklendi

--- lib/providers/categories__provider.dart
+++ lib/providers/categories__provider.dart
@@ -6,9 +6,10 @@ final categoriesProvider = Provider((ProviderRef ref) {
   return const [
     Category(id: "1", name: "Başlangıçlar", color: Colors.purple),
     Category(id: "2", name: "Ara Sıcaklar", color: Colors.pink),
-    Category(id: "3", name: "Ana Yemekler", color: Colors.yellow),
-    Category(id: "4", name: "Tatlılar", color: Colors.red),
+    Category(id: "3", name: "Ana Yemekler", color: Colors.red),
+    Category(id: "4", name: "Tatlılar", color: Colors.yellow),
     Category(id: "5", name: "Mezeler", color: Colors.green),
+    Category(id: "5", name: "İçecekler", color: Colors.blue),
   ];
 });
 
